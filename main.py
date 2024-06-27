6c702ff92cb8705157053a46280596018a027b7e
Daniel <dborrego909@gmail.com>
2024-06-26 22:40:42 -0700

    Imported sys

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import csv
+import sys
 
-topic = input("What did you study?")
-time = input("How long did you study for?")
 
-with open("practice.csv", "a") as file:
-    writer = csv.DictWriter(file, fieldnames=["topic", "time")
-    writer.writerow({"topic": topic, "time": time})
+def main():
+    topic = input("What did you study?: ")
+    time = input("How long did you study for?: ")
+
+    with open(f"{topic}.csv", "a") as file:
+        writer = csv.DictWriter(file, fieldnames=["topic", "time"])
+        writer.writerow({"topic": topic, "time": time})
